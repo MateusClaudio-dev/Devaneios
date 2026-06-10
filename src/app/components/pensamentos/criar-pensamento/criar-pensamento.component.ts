6b42085adc9902a4c19e04083019126853ad457a
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-09 21:13:54 -0300

    add components

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendodo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert('Pensameto deve ser criado');
+  }
+
+  cancelarPensamento() {
+    alert('Pensamento deve ser cancelado')
+  }
+}
